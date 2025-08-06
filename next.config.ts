a5f08f1e0877c47bcfd0f689336286ecabefd353
jaksoftwares <jaksoftwares05@gmail.com>
2025-08-06 13:58:40 +0300

    updated next config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    // add any experimental features you use
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...(config.externals || []),
+        {
+          'next-seo': 'next-seo',
+        },
+      ];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
